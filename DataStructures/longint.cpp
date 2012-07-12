345368e5e8fd5ec58d6ff8a9fb953f5a1eae74c5
Bernhard Brodowsky <brodowsb@ethz.ch>
2012-07-12 13:49:31 +0200

    fixed bitwise or

--- DataStructures/longint.cpp
+++ DataStructures/longint.cpp
@@ -549,10 +549,10 @@ namespace DataStructures {
 
   LongInt& LongInt::operator|=(const LongInt& other)
   {
-    part_type keep = 0;
-    part_type other_keep = 0;
-    part_type new_keep = 0;
     bool new_positive = m_positive && other.m_positive;
+    part_type keep = !m_positive;
+    part_type other_keep = !other.m_positive;
+    part_type new_keep = !new_positive;
     for (index_type i = 0; i < std::max(size(), other.size()); ++i) {
       part_type part, other_part;
       if (m_positive) {
@@ -560,21 +560,27 @@ namespace DataStructures {
       } else {
         part_type tmp = (i < size() ? ~m_content[i] : (1ul << PART_SIZE) - 1) + keep;
         part = lower_half(tmp);
-        keep = upper_half(tmp);
+        keep = ~upper_half(tmp);
       }
       if (other.m_positive) {
         other_part = i < other.size() ? other.m_content[i] : 0;
       } else {
         part_type tmp = (i < other.size() ? ~other.m_content[i] : (1ul << PART_SIZE) - 1) + other_keep;
         other_part = lower_half(tmp);
-        other_keep = upper_half(tmp);
+        other_keep = ~upper_half(tmp);
       }
+      part_type new_part;
       if (new_positive) {
-        m_content[i] = part | other_part;
+        new_part = part | other_part;
       } else {
         part_type tmp = ~(part | other_part) + new_keep;
-        m_content[i] = lower_half(tmp);
-        new_keep = upper_half(tmp);
+        new_part = lower_half(tmp);
+        new_keep = ~upper_half(tmp);
+      }
+      if (i >= size()) {
+        m_content.push(new_part);
+      } else {
+        m_content[i] = new_part;
       }
     }
     m_positive = new_positive;
